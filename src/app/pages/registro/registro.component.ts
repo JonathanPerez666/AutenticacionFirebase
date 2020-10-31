4a636be48f6a8e5be2863ce1e0bc7ae07904587e
Jonathan <mcjhona069@gmail.com>
2020-10-30 19:57:01 -0600

    Registrar un nuevo usuario

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from '../../models/usuario.model';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-registro',
@@ -11,7 +12,7 @@ export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() { 
     this.usuario = new UsuarioModel();
@@ -19,7 +20,13 @@ export class RegistroComponent implements OnInit {
 
   onSubmit(form: NgForm ) {
     if (form.invalid) { return; }
-    console.log(form);
+
+    this.auth.nuevousuario(this.usuario)
+    .subscribe(resp => {
+      console.log(resp);
+    }, (err) => {
+      console.log(err.error.error.message);
+    });
 
   }
 }
