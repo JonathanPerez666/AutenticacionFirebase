36c5dfa9a1c3dbfd04a01e8060dc327e81d26dfc
Jonathan <mcjhona069@gmail.com>
2020-10-30 20:16:15 -0600

    Recordar usuario

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -13,6 +13,7 @@ import { Router } from '@angular/router';
 export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
+  recordarme = false;
 
   constructor(private auth: AuthService,
     private router: Router) { }
@@ -25,7 +26,7 @@ export class RegistroComponent implements OnInit {
     if (form.invalid) { return; }
 
     swal({
-      button: false,
+      buttons:{value: false},
       icon: 'info',
       text: 'Espere por favor...',
       closeOnClickOutside: false,
@@ -35,6 +36,9 @@ export class RegistroComponent implements OnInit {
     .subscribe(resp => {
       console.log(resp);
       swal.close();
+      if (this.recordarme) {
+        localStorage.setItem('email', this.usuario.email);
+      }
       this.router.navigateByUrl('/home');
     }, (err) => {
       console.log(err.error.error.message);
