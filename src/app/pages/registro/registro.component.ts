9ea4e29a38cf5766ec3ac3b258c134a020f4f907
Jonathan <mcjhona069@gmail.com>
2020-10-30 20:09:48 -0600

    Sweetalert - Mostrar notificaciones al usuario

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from '../../models/usuario.model';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import swal from 'sweetalert';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -12,7 +14,8 @@ export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,
+    private router: Router) { }
 
   ngOnInit() { 
     this.usuario = new UsuarioModel();
@@ -21,11 +24,25 @@ export class RegistroComponent implements OnInit {
   onSubmit(form: NgForm ) {
     if (form.invalid) { return; }
 
+    swal({
+      button: false,
+      icon: 'info',
+      text: 'Espere por favor...',
+      closeOnClickOutside: false,
+    });
+
     this.auth.nuevousuario(this.usuario)
     .subscribe(resp => {
       console.log(resp);
+      swal.close();
+      this.router.navigateByUrl('/home');
     }, (err) => {
       console.log(err.error.error.message);
+      swal({
+        icon: 'error',
+        title: 'Error al registrar',
+        text: err.error.error.message
+      });
     });
 
   }
